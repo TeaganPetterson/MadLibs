797c3fadbac77ac04b4f481fbd7efb30dfe487c7
Teagan Petterson <teagan.petterson1@gmail.com>
2023-05-09 18:14:10 -0600

    Finished with Day 1

---
+++ madlibs.py
@@ -0,0 +1,86 @@
+"""A madlib game that compliments its users."""
+
+from random import choice
+
+from flask import Flask, render_template, request
+
+# "__name__" is a special Python variable for the name of the current module.
+# Flask wants to know this to know what any imported things are relative to.
+app = Flask(__name__)
+
+AWESOMENESS = [
+    "awesome",
+    "terrific",
+    "fantastic",
+    "neato",
+    "fantabulous",
+    "wowza",
+    "oh-so-not-meh",
+    "brilliant",
+    "ducky",
+    "coolio",
+    "incredible",
+    "wonderful",
+    "smashing",
+    "lovely",
+]
+
+
+@app.route("/")
+def start_here():
+    """Display homepage."""
+
+    return "<a href='/hello'>Hi this is the home page</a>"
+
+
+@app.route("/hello")
+def say_hello():
+    """Say hello to user."""
+
+    return render_template("hello.html")
+
+
+@app.route("/greet")
+def greet_person():
+    """Greet user with compliment."""
+
+    player = request.args.get("person")
+    compliment = choice(AWESOMENESS)
+    # playAGame = request.args.get("playAGame")
+
+    # if playAGame == "y": 
+    #     return render_template("game.html")
+
+    return render_template("compliment.html", person=player, compliment=compliment)
+
+@app.route("/game")
+def show_madlib_form():
+    """play a game if the user so chooses"""
+
+    playAGame = request.args.get("playAGame")
+
+    if playAGame == "Yes": 
+        return render_template("game.html")
+    else:
+        return render_template("goodbye.html")
+
+@app.route("/madlib")
+def show_madlib():
+
+    # color = 
+    # noun = 
+    # person = 
+    # adjective = 
+    return render_template(
+        "madlib.html", 
+        color = request.args.get("color"), 
+        noun = request.args.get("noun"), 
+        person = request.args.get("person"), 
+        adjective = request.args.get("adjective")
+)
+
+if __name__ == "__main__":
+    # Setting debug=True gives us error messages in the browser and also
+    # "reloads" our web app if we change the code.
+
+    app.run(debug=True, host="0.0.0.0", port="3000")
